18b58789864e0e2b6abbf9b4582848148cde40b5
fungirak <gabilazzarini88@outlook.com>
2024-01-10 16:14:50 -0300

    Arreglado problema de windows is not defined,next, materialize y otras yerbas serranas

--- next.config.js
+++ next.config.js
@@ -2,4 +2,6 @@ const disableSSR = process.env.NEXT_PUBLIC_DISABLE_SSR === 'true';
 
 module.exports = {
   reactStrictMode: true,
+  basePath: '/intento-de-migracion-personal-website',
+  assetPrefix: '/intento-de-migracion-personal-website/',
 };
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,13 +4,19 @@ import Document, { Html, Head, Main, NextScript } from 'next/document';
 export default class MyDocument extends Document {
   render() {
     return (
-      <Html lang="es">
-        <Head />
-        <body>
-          <Main />
-          <NextScript />
-        </body>
+      <Html lang="en">
+          <Head>
+              <link
+                  rel="stylesheet"
+                  href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css"
+              />
+          </Head>
+          <body>
+              <Main />
+              <NextScript />
+              <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+          </body>
       </Html>
-    );
+  );
   }
 }
