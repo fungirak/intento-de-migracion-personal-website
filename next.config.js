1d6178a3270e1ef04998f9fef952dec6ad245591
fungirak <gabilazzarini88@outlook.com>
2024-01-11 09:14:12 -0300

    Evita desplazamiento horizontal - producción

--- next.config.js
+++ next.config.js
@@ -2,4 +2,15 @@ const disableSSR = process.env.NEXT_PUBLIC_DISABLE_SSR === 'true';
 
 module.exports = {
   reactStrictMode: true,
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      // Configuración del lado del cliente
+      config.resolve.fallback = {
+        fs: false,
+      };
+    }
+
+    return config;
+  },
+ 
 };
